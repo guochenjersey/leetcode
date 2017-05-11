8134a5a169dcbf8c354d86764910dcfc7bf13f55
Guo chen <chen.guo@flextrade.com>
2017-05-11 14:07:01 -0400

    accepted 61

---
+++ Java/src/main/java/org/black/lotus/Kata148.java
@@ -0,0 +1,13 @@
+package org.black.lotus;
+
+
+import org.black.lotus.org.black.lotus.support.ListNode;
+
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ * */
+public class Kata148 {
+    public ListNode sortList(ListNode head) {
+        return null;
+    }
+}
---
+++ Java/src/main/java/org/black/lotus/Kata572.java
@@ -0,0 +1,29 @@
+package org.black.lotus;
+
+import org.black.lotus.org.black.lotus.support.TreeNode;
+
+public class Kata572 {
+
+    // pre-order traverse to compare
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+
+        return isSame(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    public boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+
+        if (s.val != t.val)
+            return false;
+
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
