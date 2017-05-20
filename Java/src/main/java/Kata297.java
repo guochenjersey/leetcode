037fffb5eafe65b718542869ec15d1b6e49c385d
Guo chen <chen.guo@flextrade.com>
2017-05-19 20:54:33 -0400

    temp commit

---
+++ Java/src/main/java/Kata297.java
@@ -0,0 +1,15 @@
+import org.black.lotus.org.black.lotus.support.TreeNode;
+
+public class Kata297 {
+    private static String DELIMITER = ";";
+    private static String NULL = "null";
+
+    public String serialize(TreeNode root) {
+        return null;
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data == null || data.length() == 0) return null;
+        return null;
+    }
+}
