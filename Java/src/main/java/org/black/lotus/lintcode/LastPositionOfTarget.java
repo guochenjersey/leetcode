1a88b4ecb021f91f69ec33e9e081c9430e401ef8
Guo chen <chen.guo@flextrade.com>
2017-12-15 13:03:11 -0500

    update

--- Java/src/main/java/org/black/lotus/lintcode/LastPositionOfTarget.java
+++ Java/src/main/java/org/black/lotus/lintcode/LastPositionOfTarget.java
@@ -2,48 +2,36 @@ package org.black.lotus.lintcode;
 
 public class LastPositionOfTarget {
 
+    /**
+     * In a sorted array, which I assume ascending order here.
+     * */
     public int lastPosition(int[] source, int target) {
-        if (source == null || source.length == 0
-                || target < source[0] || target > source[source.length - 1]) {
+        if (source == null || source.length == 0) {
             return -1;
         }
 
-        if (target == source[source.length - 1]) {
-            return source.length - 1;
-        }
+        int start = 0, end = source.length - 1;
 
-        if (target == source[0]) {
-            return 0;
-        }
-
-        int lowerBoundIndex = 0;
-        int upperBoundIndex = source.length - 1;
-        int lastFoundPos = -1;
-        while (lowerBoundIndex < upperBoundIndex) {
-            int middle = findMiddle(lowerBoundIndex, upperBoundIndex);
-            if (source[middle] == target) {
-                lastFoundPos = middle;
-                lowerBoundIndex = middle;
-            } else if (target > source[lowerBoundIndex] && target < source[middle] && (middle - lowerBoundIndex > 1)) {
-                upperBoundIndex = middle;
-            } else if (target < source[upperBoundIndex] && target > source[middle] && (upperBoundIndex - middle) > 1) {
-                lowerBoundIndex = middle;
-            } else {
-                break;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (target == source[mid]) {
+                start = mid;
+            } else if (target > source[mid]) {
+                start = mid;
+            } else if (target < source[mid]) {
+                end = mid;
             }
         }
 
-        return lastFoundPos;
+        if (target == source[end]) {
+            return end;
+        }
+
+        if (target == source[start]) {
+            return start;
+        }
+
+        return -1;
     }
 
-    private int findMiddle(int lowerBoundIndex, int upperBoundIndex) {
-        return ((upperBoundIndex - lowerBoundIndex) / 2 ) + lowerBoundIndex;
-    }
-
-    public static void main(String... args) {
-        int[] source = {1,2,4,5,5,6,6,6};
-        LastPositionOfTarget lastPositionOfTarget = new LastPositionOfTarget();
-        int lastFoundPos = lastPositionOfTarget.lastPosition(source, 1000);
-        System.out.println(lastFoundPos);
-    }
 }
---
+++ Java/src/test/java/org/black/lotus/lintcode/LastPositionOfTargetTest.java
@@ -0,0 +1,40 @@
+package org.black.lotus.lintcode;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import static org.junit.Assert.*;
+
+public class LastPositionOfTargetTest {
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(2);
+
+    private LastPositionOfTarget lastPositionOfTarget;
+
+    @Before
+    public void setup() {
+        lastPositionOfTarget = new LastPositionOfTarget();
+    }
+
+    @Test
+    public void should_handle_empty_array() {
+        int[] nums = null;
+        int result = lastPositionOfTarget.lastPosition(nums, 1);
+        assertThat(-1, CoreMatchers.equalTo(result));
+
+        nums = new int[0];
+        result = lastPositionOfTarget.lastPosition(nums, 1);
+        assertThat(-1, CoreMatchers.equalTo(result));
+    }
+
+    @Test
+    public void should_able_to_find_last_pos() {
+        int[] nums = {0, 1, 2, 3, 4, 5, 5, 5, 8, 9, 10};
+        int result = lastPositionOfTarget.lastPosition(nums, 5);
+        assertThat(7, CoreMatchers.equalTo(result));
+    }
+
+}
