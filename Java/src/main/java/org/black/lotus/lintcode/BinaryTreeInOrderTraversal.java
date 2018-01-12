db12ca6c0038ad1cd3db687e4aa7a846eeb56538
Guo chen <chen.guo@flextrade.com>
2018-01-11 21:50:12 -0500

    update

---
+++ Java/src/main/java/org/black/lotus/lintcode/BinaryTreeInOrderTraversal.java
@@ -0,0 +1,23 @@
+package org.black.lotus.lintcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeInOrderTraversal {
+
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> res = new LinkedList<>();
+    helper(root, res);
+    return res;
+  }
+
+  private void helper(TreeNode node, List<Integer> path) {
+    if (node == null) {
+      return;
+    }
+
+    helper(node.left, path);
+    path.add(node.val);
+    helper(node.right, path);
+  }
+}
