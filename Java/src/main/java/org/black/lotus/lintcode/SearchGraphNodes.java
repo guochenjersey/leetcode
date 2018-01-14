67c30131eacc48dd35d15d49cb2d374747fb18ce
Guo chen <chen.guo@flextrade.com>
2018-01-13 23:02:23 -0500

    Start search graph nodes

---
+++ Java/src/main/java/org/black/lotus/lintcode/SearchGraphNodes.java
@@ -0,0 +1,26 @@
+package org.black.lotus.lintcode;
+
+
+import java.util.ArrayList;
+import java.util.Map;
+
+class UndirectedGraphNode {
+  int label;
+  ArrayList<UndirectedGraphNode> neighbors;
+
+  UndirectedGraphNode(int x) {
+    label = x;
+    neighbors = new ArrayList<UndirectedGraphNode>();
+  }
+}
+
+
+public class SearchGraphNodes {
+
+  public UndirectedGraphNode searchNode(ArrayList<UndirectedGraphNode> graph,
+      Map<UndirectedGraphNode, Integer> values,
+      UndirectedGraphNode node,
+      int target) {
+    return null;
+  }
+}
