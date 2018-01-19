9bd9696272af4421f835afce9c73d55fe74086e4
Guo chen <chen.guo@flextrade.com>
2018-01-18 23:12:40 -0500

    update

---
+++ Java/src/main/java/org/black/lotus/lintcode/CombinationSumII.java
@@ -0,0 +1,15 @@
+package org.black.lotus.lintcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSumII {
+
+  public List<List<Integer>> combinationSum2(int[] num, int target) {
+    List<List<Integer>> results = new ArrayList<>();
+
+    return results;
+  }
+
+  
+}
