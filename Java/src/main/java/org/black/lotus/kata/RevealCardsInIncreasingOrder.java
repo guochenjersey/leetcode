7f0932cd783836dbfcbb2588d8e6d2749d9a904d
Guo chen <chen.guo@flextrade.com>
2019-02-22 15:40:49 -0500

    update

---
+++ Java/src/main/java/org/black/lotus/kata/RevealCardsInIncreasingOrder.java
@@ -0,0 +1,14 @@
+package org.black.lotus.kata;
+
+import org.black.lotus.marker.FirstRound;
+import org.black.lotus.marker.LeetCode;
+import org.black.lotus.marker.Medium;
+
+/**
+ *
+ * */
+@LeetCode
+@Medium
+@FirstRound
+public class RevealCardsInIncreasingOrder {
+}
