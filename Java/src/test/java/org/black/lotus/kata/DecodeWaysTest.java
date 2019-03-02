2b08d0446d3f1af1bd75792da6fc4e39d16a3f27
Guo chen <chen.guo@flextrade.com>
2019-03-01 21:40:50 -0500

    update

---
+++ Java/src/test/java/org/black/lotus/kata/DecodeWaysTest.java
@@ -0,0 +1,19 @@
+package org.black.lotus.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DecodeWaysTest {
+
+    @Test
+    public void should_decode_ways() {
+        DecodeWays decodeWays = new DecodeWays();
+        assertEquals(2, decodeWays.numDecodings("12"));
+        assertEquals(3, decodeWays.numDecodings("226"));
+        assertEquals(0, decodeWays.numDecodings("0"));
+        assertEquals(1, decodeWays.numDecodings("10"));
+        assertEquals(0, decodeWays.numDecodings("50"));
+        assertEquals(0, decodeWays.numDecodings("01"));
+    }
+}
